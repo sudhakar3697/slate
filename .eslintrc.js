b08586d35600950e12a5b20648959b35630de963
rsudhakar3697 <rsudhakar3697@gmail.com>
2020-06-27 15:42:53 +0530

    Added ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:security/recommended',
+    'plugin:node/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'node/exports-style': ['error', 'module.exports'],
+    'node/file-extension-in-import': ['error', 'always'],
+    'node/prefer-global/buffer': ['error', 'always'],
+    'node/prefer-global/console': ['error', 'always'],
+    'node/prefer-global/process': ['error', 'always'],
+    'node/prefer-global/url-search-params': ['error', 'always'],
+    'node/prefer-global/url': ['error', 'always'],
+    'node/prefer-promises/dns': 'error',
+    'node/prefer-promises/fs': 'error',
+  },
+  plugins: [
+    'security',
+  ],
+};
