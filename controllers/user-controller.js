b50f0af8a61dd448ec69b529ab0a71d40391519c
MadhanSawyer <madhankumar4668@gmail.com>
2020-07-01 22:06:36 +0530

    Added Reset Password Feature

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -107,7 +107,7 @@ module.exports = {
         res.send('Verification email expired. Resend confirmation email to verify.');
       } else {
         const record = await userInfo.findByPk(userId);
-        record.update({ verified: true });
+        await record.update({ verified: true });
         res.send('Successfully verified your account. You may close this window.');
       }
     } catch (err) {
@@ -122,6 +122,69 @@ module.exports = {
       res.send('Confirmation mail has been sent');
     } catch (err) {
       res.status(400).send('Confirmation mail not sent');
+    }
+  },
+
+  sendResetPasswordEmail: async (req, res) => {
+    try {
+      const record = await userInfo.findOne({ where: { email: req.body.email } });
+      const token = encodeRegistrationToken(record.id);
+      const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: config.CONFIRMATION_MAIL,
+          pass: config.CONFIRMATION_MAIL_PASSWORD,
+        },
+      });
+
+      const mailOptions = {
+        from: `"Slate - Online Clipboard" <${config.CONFIRMATION_MAIL}>`,
+        to: req.body.email,
+        subject: 'Reset your Slate Account Password',
+        html: `<a href='http://localhost:${config.PORT}/users/password-reset?token=${token}'>Click here to reset your password</a>`,
+      };
+      const response = await transporter.sendMail(mailOptions);
+      logger.info(response.messageId);
+      res.send('Reset password mail has been sent');
+    } catch (err) {
+      res.status(404).send('Account does not exist');
+    }
+  },
+  showResetPasswordUI: async (req, res) => {
+    res.send(`<!DOCTYPE html>
+      <html>
+      <head>
+          <title>Slate - Change Password</title>
+      </head>
+      <body>
+          New Password: <input type="password" id="new-password">
+         <!-- Confirm Password:<input type="password" id="confirm-password"> -->
+          <button onclick="changePassword()">Reset password</button>
+          <script>
+              async function changePassword() {
+                  let response = await fetch('http://localhost:${config.PORT}/users/password-reset', {
+                      method: 'PATCH',
+                      headers: {
+                          'Content-Type': 'application/json',
+                      },
+                      body: JSON.stringify({ token:'${req.query.token}',password:document.getElementById('new-password').value })
+              });
+              response = await response.text();
+              alert(response);
+      }
+          </script>
+      </body>
+      </html>`);
+  },
+  resetPassword: async (req, res) => {
+    try {
+      const { token, password } = req.body;
+      const record = await userInfo.findByPk(decodeRegistrationToken(token).userId);
+      const encryptedPassword = await bcrypt.hash(password, config.SALT_ROUNDS);
+      await record.update({ password: encryptedPassword });
+      res.send('Password changed successfully');
+    } catch (err) {
+      res.status(400).send(err.message);
     }
   },
 };
